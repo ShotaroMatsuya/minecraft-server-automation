fe3df3255ae35715a73d6c58ec84c1ffba7111be
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-24 07:44:53 +0900

    feat: create root module

---
+++ terraform/terraform.auto.tfvars
@@ -0,0 +1,5 @@
+# Generic Variables
+aws_region     = "ap-northeast-1"
+owners         = "minecraft"
+environment    = "test"
+aws_account_id = "528163014577"
---
+++ terraform/versions.tf
@@ -0,0 +1,39 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.2"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {
+    bucket = "minecraft-backend"
+    key    = "state/terraform.tfstate"
+    region = "ap-northeast-1"
+    # For State Locking
+    dynamodb_table = "minecraft-statelocking"
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region = var.aws_region
+}
+# Create a Null Resource and provisioners
+resource "null_resource" "name" {
+  # Local Exec Provisioner: local-exec provisioner (Create-Time Provisioner - Triggered during Create Resource)
+  provisioner "local-exec" {
+    command     = "echo VPC created on `date` and VPC ID: ${module.custom_vpc.vpc_id} >> creation-time-vpc-id.txt"
+    working_dir = "local-exec-output-files/"
+  }
+}
