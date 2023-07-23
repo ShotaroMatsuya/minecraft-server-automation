1736a6d9ea98973416b34b25709288b5d20f3b0a
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-24 07:41:29 +0900

    feat: add vpc module

---
+++ terraform/modules/vpc/route53.tf
@@ -0,0 +1,16 @@
+# Get DNS information from AWS Route53
+data "aws_route53_zone" "mydomain" {
+  name = "smat710.tk"
+}
+
+# DNS Registration 
+resource "aws_route53_record" "apps_dns" {
+  zone_id = data.aws_route53_zone.mydomain.zone_id
+  name    = "minecraft.smat710.tk"
+  type    = "A"
+  alias {
+    name                   = var.nlb_dns_name
+    zone_id                = var.nlb_zone_id
+    evaluate_target_health = true
+  }
+}
---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,56 @@
+############################
+# VPC Input Variables      #
+############################
+# VPC Name
+variable "vpc_name" {
+  description = "VPC Name"
+  type        = string
+}
+
+# VPC CIDR Block
+variable "vpc_cidr_block" {
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+# VPC Availability Zones
+variable "vpc_availability_zones" {
+  description = "VPC Availability Zones"
+  type        = list(string)
+}
+
+# VPC Public Subnets
+variable "vpc_public_subnets" {
+  description = "VPC Public Subnets"
+  type        = list(string)
+}
+
+variable "sg_rule_for_alb" {
+  description = "SG inbound rule for ALB"
+  type        = list(map(any))
+  default     = []
+}
+
+variable "nlb_dns_name" {
+  description = "DNS Name of nlb to register in route53"
+  type        = string
+}
+
+variable "nlb_zone_id" {
+  description = "nlb zone id to register in route53"
+  type        = string
+}
+
+
+variable "owners" {}
+variable "environment" {}
+
+locals {
+  owners      = var.owners
+  environment = var.environment
+  name        = "${var.owners}-${var.environment}"
+  common_tags = {
+    owners      = local.owners
+    environment = local.environment
+  }
+}
