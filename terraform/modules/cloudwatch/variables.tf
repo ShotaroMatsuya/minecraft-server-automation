c34da41815c38f9ff644825a24cdd46615c3add9
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-07-26 12:48:14 +0900

    feat: create cloudwatch module

--- terraform/modules/cloudwatch/variables.tf
+++ terraform/modules/cloudwatch/variables.tf
@@ -2,6 +2,20 @@ variable "firelens_log_group" {
   type = string
 }
 
+variable "ecs_cluster" {
+  type        = string
+  description = "Name of the cluster."
+}
+variable "ecs_service" {
+  type        = string
+  description = "Name of the service."
+}
+
+
+variable "sns_topic_arn" {
+  description = "ARN of the SNS topic to subscribe to."
+}
+
 variable "owners" {}
 variable "environment" {}
 
