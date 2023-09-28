c5d90ed24c82fe07cef6c3f83089e06fad92aeb5
Shotaro Matsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-09-28 19:29:28 +0900

    fix: small fix

--- terraform/modules/iam/outputs.tf
+++ terraform/modules/iam/outputs.tf
@@ -4,9 +4,19 @@ output "ecs_task_role_arn" {
   value       = aws_iam_role.main_ecs_tasks.arn
 }
 
+output "ecs_task_role_name" {
+  description = "The Name of ecs task role"
+  value = aws_iam_role.main_ecs_tasks.name
+}
+
 output "ecs_task_execution_role_arn" {
   description = "The ARN of ecs task execution role"
   value       = aws_iam_role.task_execution_role.arn
+}
+
+output "ecs_task_execution_role_name" {
+  description = "The Name of ecs task execution role"
+  value = aws_iam_role.task_execution_role.name
 }
 
 output "chatbot_notification_role_arn" {
--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -62,18 +62,3 @@ output "allow_nfs_sg_group_name" {
   description = "The name of the security group"
   value       = module.allow_nfs_sg.security_group_name
 }
-
-################################
-# Output MyDomain Zone ID      #
-################################
-output "mydomain_zoneid" {
-  description = "The Hosted Zone id of the desired Hosted Zone"
-  value       = data.aws_route53_zone.mydomain.zone_id
-}
-##############################
-# Output MyDomain name       #
-##############################
-output "mydomain_name" {
-  description = " The Hosted Zone name of the desired Hosted Zone."
-  value       = data.aws_route53_zone.mydomain.name
-}
