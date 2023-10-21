285f28c6de9157b84b097a2652bcfbb13147a164
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-10-22 06:32:12 +0900

    fix: to perform file operations within VOLUME

--- docker/minecraft/scripts/entrypoint.sh
+++ docker/minecraft/scripts/entrypoint.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+# Volumeにworldを移動する
+cp -r /world /data/
+chown minecraft:minecraft /data/world -R
+
 export S3_BUCKET=$S3_BUCKET_NAME # minecraft-backend
 S3_PREFIX=$S3_PREFIX_NAME # backups
 
@@ -14,7 +18,7 @@ cleanup() {
     if [ ! -d backup/${PARTITION_DATE} ]; then
         mkdir -p backup/${PARTITION_DATE}
     fi
-    aws s3 rm s3://${S3_BUCKET}/${S3_PREFIX}/${PARTITION_DATE} --recursive
+    # aws s3 rm s3://${S3_BUCKET}/${S3_PREFIX}/${PARTITION_DATE} --recursive
     FILE_NAME='minecraft-'${BACKUP_DATE_TIME}'.tar.gz'
     tar -zcvf backup/${PARTITION_DATE}/${FILE_NAME} -C /data/ world/
     aws s3 cp backup/${PARTITION_DATE}/${FILE_NAME} s3://${S3_BUCKET}/${S3_PREFIX}/${PARTITION_DATE}/
@@ -23,14 +27,13 @@ cleanup() {
 }
 
 # function executed when container is started
-start() {
     echo "Container is starting. Downloading data from S3..."
     LATEST_BACKUP=$(aws s3 ls --recursive s3://${S3_BUCKET}/${S3_PREFIX}/ | sort | tail -n 1 | awk '{print $4}' )
     # donwload s3 and unzip it to /data/world/
     aws s3 cp s3://${S3_BUCKET}/${LATEST_BACKUP} /data/world/
     find /data/world/ -name "*.tar.gz" -exec tar -xvf {} \;
     rm -rf /data/world/*.tar.gz
-}
+
 
 # trap SIGTERM signal and call cleanup
 trap cleanup TERM
