fb48aea8f82fc9096585d54b853f6ddea44ebb28
Shotaro Matsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2023-11-03 10:22:19 +0900

    chore: modify messages

--- terraform/scripts/send_to_slack.sh
+++ terraform/scripts/send_to_slack.sh
@@ -4,7 +4,7 @@ WEBHOOK_URL=$1
 ALB_DNS_NAME=$2
 
 payload="{
-    \"text\": \"アドレスが発行されました。以下のdomainにログインしてください。\n\n ** $ALB_DNS_NAME **\"
+    \"text\": \":tada::tada:アドレスが発行されました。以下のdomainにログインしてください。:tada::tada:\n\n * $ALB_DNS_NAME *\"
 }"
 
 curl -X POST -H 'Content-Type: application/json' --data "$payload" $WEBHOOK_URL
